84e88dba6e3e36a0ef60e5067d010164166f3184
unknown <pawel.wojtanowicz@gmail.com>
2022-06-05 21:26:29 +0200

    begining of the app

---
+++ src/components/UserList.tsx
@@ -0,0 +1,15 @@
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+
+export default function UserList() {
+  return (
+    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }} align="left" ml={9.5}>
+            User list
+    </Typography>
+   
+  );
+}
