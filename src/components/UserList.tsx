956e8f427c75c90a9e7054766ff662ac5f58cac2
unknown <pawel.wojtanowicz@gmail.com>
2022-06-05 21:49:18 +0200

    ffff

--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -4,12 +4,21 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
+import { Button } from '@mui/material';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
 
 export default function UserList() {
   return (
-    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }} align="left" ml={9.5}>
-            User list
+    <Box ml={9.5} textAlign="left">
+    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+      User list
     </Typography>
-   
+    <Box>This is user list</Box>
+    <Button variant="contained">Add user (+)</Button>
+    
+    </Box>
   );
 }
